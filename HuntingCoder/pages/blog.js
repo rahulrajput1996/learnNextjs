1a035e98549120e3e337e05b43102dd9527714f5
rahulkumarsingh <ra10011996@gmail.com>
2024-01-16 00:23:54 +0530

    prerendering start

--- HuntingCoder/pages/blog.js
+++ HuntingCoder/pages/blog.js
@@ -1,42 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link'
-import styles from "../styles/Blog.module.css"
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import styles from "../styles/Blog.module.css";
 
 const Blog = () => {
-    const [allFiles, setAllFiles] = useState([]);
-    const [allFilesData, setAllFilesData] = useState([]);
+  const [allFiles, setAllFiles] = useState([]);
+  const [allFilesData, setAllFilesData] = useState([]);
 
-    const fetchAllFilesName = async () => {
-        try {
-            let data1 = await fetch("http://localhost:3000/api/blogs");
-            let data2 = await fetch("http://localhost:3000/api/allBlogs");
-            let json1 = await data1.json();
-            let json2 = await data2.json();
-            setAllFiles(json1);
-            setAllFilesData(json2);
-        } catch (error) {
-            console.log(error.message)
-        }
-    }
+  const fetchAllFilesName = async () => {
+    try {
+      let data1 = await fetch("http://localhost:3000/api/blogs");
+      let data2 = await fetch("http://localhost:3000/api/allBlogs");
+      let json1 = await data1.json();
+      let json2 = await data2.json();
+      setAllFiles(json1);
+      setAllFilesData(json2);
+    } catch (error) {}
+  };
 
-    useEffect(() => {
-        fetchAllFilesName();
-    }, [])
+  useEffect(() => {
+    fetchAllFilesName();
+  }, []);
 
-    return (
-        <>
-            <div className={styles.container}>
-                {allFiles?.map((item, index) => {
-                    return (
-                        <div key={index}>
-                            <Link href={`/blogpost/${item}`} className={styles.contchild}><h3 >{allFilesData[index].title}</h3></Link>
-                            <span>{allFilesData[index].content}</span>
-                        </div>
-                    )
-                })}
+  return (
+    <>
+      <div className={styles.container}>
+        {allFiles?.map((item, index) => {
+          return (
+            <div key={index}>
+              <Link href={`/blogpost/${item}`} className={styles.contchild}>
+                <h3>{allFilesData[index].title}</h3>
+              </Link>
+              <span>{allFilesData[index].content}</span>
             </div>
-        </>
-    )
-}
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-export default Blog
+export default Blog;
--- HuntingCoder/pages/blogpost/[slug].js
+++ HuntingCoder/pages/blogpost/[slug].js
@@ -1,40 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router'
-import styles from "../../styles/Blog.module.css"
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import styles from "../../styles/Blog.module.css";
 
 const Myslog = () => {
-    const router = useRouter();
-    console.log(router)
-    const { slug } = router.query;
-    const [contentData, setContentData] = useState(null);
+  const router = useRouter();
+  const { slug } = router.query;
+  const [contentData, setContentData] = useState(null);
 
-    const readFileSystem = async () => {
-        try {
-            let data = await fetch(`http://localhost:3000/api/blog?title=${slug}`);
-            let response = await data.json()
-            setContentData(response)
-        } catch (error) {
-        }
+  const readFileSystem = async () => {
+    try {
+      let data = await fetch(`http://localhost:3000/api/blog?title=${slug}`);
+      let response = await data.json();
+      setContentData(response);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    if (router.isReady) {
+      readFileSystem();
     }
+  }, [router.isReady]);
 
-    useEffect(() => {
-        if (router.isReady) {
-            readFileSystem();
-        }
-    }, [router.isReady])
+  return (
+    <>
+      <div className={styles.container}>
+        <div>
+          <h3 className={styles.contchild}>{slug}</h3>
+          <h3 className={styles.contchild}>{contentData?.title}</h3>
+          <span>{contentData?.content}</span>
+          <h3 style={{ textAlign: "center" }}>{contentData?.author}</h3>
+        </div>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <div className={styles.container}>
-                <div >
-                    <h3 className={styles.contchild}>{slug}</h3>
-                    <h3 className={styles.contchild}>{contentData?.title}</h3>
-                    <span>{contentData?.content}</span>
-                    <h3 style={{ textAlign: "center" }}>{contentData?.author}</h3>
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default Myslog
+export default Myslog;
